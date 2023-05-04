5a9e85b77b9329e7290f7b4d675c50936cf22a96
Guillaume <guillaumelkl@gmail.com>
2023-05-04 23:26:01 +0300

    set up of back end

---
+++ DevHub-Project/server/Datatbase/connection.js
@@ -0,0 +1,15 @@
+const mongoose =require('mongoose')
+require('dotenv').config();
+
+const DATABASE_URL = process.env.DATABASE_URL;
+
+const URI = process.env.DB_URI
+
+main()
+    .then(() => console.log("db connected to successfully"))
+    .catch((err)=> console.log(err));
+    async function main() {
+        await mongoose.connect(DATABASE_URL);
+    }
+
+    module.exports =main;
---
+++ DevHub-Project/server/Server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+
+require("./Datatbase/connection")
+const port = 8080;
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+
+app.listen(port,()=>{
+    console.log(`Server is listening on port ${port}`);
+});
